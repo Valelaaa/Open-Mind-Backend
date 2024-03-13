8dbc8956181cd5d6ab364ba58eca9406c86d18bf
Valelaaa <kosneanuvaleriy@gmail.com>
2024-03-13 20:14:26 +0200

    - first commit

---
+++ src/main/kotlin/com/example/openmindbackend/OpenmindbackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.openmindbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenmindbackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenmindbackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/openmindbackend/OpenmindbackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.openmindbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenmindbackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
